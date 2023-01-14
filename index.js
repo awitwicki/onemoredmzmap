1c1eb35c4cfe22072292b8c8c34c0e885f21582d
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-01-14 15:45:36 +0100

    Add bootstrap, jquerry and update UI

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var spawnsPointMarkers = [];
+
 let mapSettings = {
     crs: L.CRS.Simple,
     minZoom: -2,
@@ -16,6 +18,7 @@ map.fitBounds(bounds);
 // map.setView([500, 500], -1.4); //set view to center around the surface layer
 
 let popup = L.popup();
+
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
@@ -28,7 +31,38 @@ map.on('click', onMapClick);
 fetch('./resources/spawns.json')
     .then((response) => response.json())
     .then((spawns) => {
-        for (let spawn of spawns) {
-            L.marker(L.latLng(spawn.coords)).addTo(map);
+        InitSpawnPointsMarkers(spawns);
+        DrawSpawnPointsOnMap();
+    });
+
+function DrawSpawnPointsOnMap() {
+    for (let spawnsPointMarker of spawnsPointMarkers) {
+        spawnsPointMarker.addTo(map);
+    }
+}
+
+function InitSpawnPointsMarkers(spawnsPoints) {
+    for (let spawn of spawnsPoints) {
+        marker = L.marker(L.latLng(spawn.coords));
+        spawnsPointMarkers.push(marker);
+    }
+}
+
+function RemoveSpawnPointMarkers() {
+    for (let spawn of spawnsPointMarkers) {
+        if(map.hasLayer(spawn)){
+            map.removeLayer(spawn)
+        }
+    }
+}
+
+$(document).ready(function(){
+    
+    $('#showSpawns').click(function() {
+        if (this.checked) {
+            DrawSpawnPointsOnMap();
+        } else {
+            RemoveSpawnPointMarkers(map);
         }
     });
+  });
